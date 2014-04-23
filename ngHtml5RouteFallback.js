0a48cec5ae3a735a3ceb8447bf333312b4cb96bf
jbachand <jeff@n2op.com>
2014-04-23 09:28:40 -0400

    adding file and bower

---
+++ ngHtml5RouteFallback.js
@@ -0,0 +1,24 @@
+/**
+ * Angular include to fix routing on pushstate failure
+ * ---------------------------
+ *
+ * Authored by  Jeff Bachand
+ *              jeff@dailybreak.com
+ *              http://www.dailybreak.com/
+ *
+ * 4/22/14 10:25 PM
+ */
+
+angular.module('ngHtml5RouteFallback', [])
+  .run([ '$window', function($window) {
+    var buggyAndroid = parseInt((/android (\d+)/.exec($window.navigator.userAgent.toLowerCase()) || [])[1], 10) < 4;
+    if (!history.pushState || buggyAndroid) {
+      if ($window.location.hash) {
+        if($window.location.pathname !== '/') $window.location.replace('/#!' + $window.location.hash.substr(2)); //Hash and a path, just keep the hash (redirect)
+      } else {
+        $window.location.replace('/#!' + $window.location.pathname); //No hash, take path
+      }
+    }
+
+  }])
+;
