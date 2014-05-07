7b0596c9214ea39a5cce87aa54315f144110f9ac
Jeff <jeff@n2op.com>
2014-05-07 15:04:05 -0400

    testing again

--- ngHtml5RouteFallback.js
+++ ngHtml5RouteFallback.js
@@ -18,6 +18,9 @@ angular.module('ngHtml5RouteFallback', [])
         if(trailingSlash !== '/'){
            $window.location.replace($window.location.pathname+'/#!' + $window.location.hash.substr(2)); //Hash and a path, just keep the hash (redirect)
          }
+         if($window.location.pathname !== '/'){
+           $window.location.replace('/#!' + $window.location.hash.substr(2)); //Hash and a path, just keep the hash (redirect)
+         }
       } else {
 
         $window.location.replace($window.location.pathname+'/#!' + $window.location.pathname); //No hash, take path
