8f42401070c8067823d5b75e7a1f3e252837792d
Jeff <jeff@n2op.com>
2014-05-06 22:29:58 -0400

    left debugging in

--- ngHtml5RouteFallback.js
+++ ngHtml5RouteFallback.js
@@ -12,7 +12,7 @@
 angular.module('ngHtml5RouteFallback', [])
   .run([ '$window', function($window) {
     var buggyAndroid = parseInt((/android (\d+)/.exec($window.navigator.userAgent.toLowerCase()) || [])[1], 10) < 4;
-    if (!history.pushState || buggyAndroid || 1==1) {
+    if (!history.pushState || buggyAndroid) {
       if ($window.location.hash) {
         if($window.location.pathname !== '/'){
            $window.location.replace($window.location.pathname+'/#!' + $window.location.hash.substr(2)); //Hash and a path, just keep the hash (redirect)
